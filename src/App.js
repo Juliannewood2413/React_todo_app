c3831720f02230c82689fb3fa77587290a39bade
Julianne Wood <juliannewood2413@gmail.com>
2021-05-27 11:12:19 -0600

    updated functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList'
@@ -6,14 +6,15 @@ import TodoList from './components/TodoList'
 function App() {
 
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
 
   return (
     <div className="App">
          <header>
         <h1>Daily Todo List</h1>
     </header>
-    <Form />
-    <TodoList />
+    <Form todos={todos} setTodos={setTodos}inputText = {inputText} setInputText = {setInputText} />
+    <TodoList todos={todos}/>
     </div>
   );
 }
